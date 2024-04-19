d276fd64558c5f5bb3a8f9b6be4857d0cdf66bb6
cr1m3s <parioleks@gmail.com>
2024-04-19 23:26:01 +0200

    +.groovy

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,61 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'OS', choices: ['linux', 'apple', 'windows'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
+    }
+
+    environment {
+        GITHUB_TOKEN=credentials('jenkins')
+        REPO = 'https://github.com/cr1m3s/kbot.git'
+        BRANCH = 'main'
+    }
+
+    stages {
+
+        stage('clone') {
+            steps {
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage('test') {
+            steps {
+                echo 'Testing started'
+                sh "make test"
+            }
+        }
+
+        stage('build') {
+            steps {
+                echo "Building binary for platform ${params.OS} on ${params.ARCH} started"
+                sh "make build GOOS=${params.OS} GOARCH=${params.ARCH}"
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo "Building image for platform ${params.OS} on ${params.ARCH} started"
+                sh "make image"
+            }
+        }
+        
+        stage('login to GHCR') {
+            steps {
+                sh "echo $GITHUB_TOKEN_PSW | docker login ghcr.io -u $GITHUB_TOKEN_USR --password-stdin"
+            }
+        }
+
+        stage('push image') {
+            steps {
+                sh "make push"
+            }
+        } 
+    }
+    post {
+        always {
+            sh 'docker logout'
+        }
+    }
+}
