e5d2b604c2cf2aac17491882b306f5fcb31f1d29
cr1m3s <parioleks@gmail.com>
2024-05-12 14:46:11 +0200

    start with monitor

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -4,6 +4,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -11,10 +12,18 @@ import (
 
 	"github.com/spf13/cobra"
 	telebot "gopkg.in/telebot.v3"
+
+	"github.com/hirosassa/zerodriver"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
 )
 
 var (
-	TeleToken = os.Getenv("TELE_TOKEN")
+	TeleToken   = os.Getenv("TELE_TOKEN")
+	MetricsHost = os.Getenv("METRICS_HOST")
 )
 
 // kbotCmd represents the kbot command
@@ -29,6 +38,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		logger := zerodriver.NewProductionLogger()
+
 		fmt.Printf("kbot %s started", appVersion)
 		kbot, err := telebot.NewBot(telebot.Settings{
 			URL:    "",
@@ -37,12 +48,16 @@ to quickly create a Cobra application.`,
 		})
 
 		if err != nil {
-			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			//log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			logger.Fatal().Str("Error", err.Error()).Msg("Please check TELE_TOKEN")
 			return
+		} else {
+			logger.Info().Str("Version", appVersion).Msg("kbot started")
 		}
 
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
+			// log.Print(m.Message().Payload, m.Text())
+			logger.Info().Str("Payload", m.Text()).Msg(m.Message().Payload)
 			payload := m.Message().Payload
 
 			switch payload {
@@ -57,7 +72,53 @@ to quickly create a Cobra application.`,
 	},
 }
 
+func initMetrics(ctx context.Context) {
+
+	if MetricsHost == "" {
+		log.Fatal("METRICS_HOST не встановлено")
+	}
+
+	exporter, err := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithEndpoint(MetricsHost),
+		otlpmetricgrpc.WithInsecure(),
+	)
+
+	if err != nil {
+		fmt.Printf("Failed to create exporter: %v\n", err)
+		panic(err)
+	}
+
+	resource := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(fmt.Sprintf("kbot_%s", appVersion)),
+	)
+
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(
+			sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(10*time.Second)),
+		),
+	)
+
+	otel.SetMeterProvider(mp)
+}
+
+func pmetrics(ctx context.Context, payload string) {
+	meter := otel.GetMeterProvider().Meter("kbot_commands_counter")
+
+	counter, err := meter.Int64Counter(fmt.Sprintf("kbot_command_%s", payload))
+	if err != nil {
+		fmt.Printf("Error creating counter: %v\n", err)
+		return
+	}
+	counter.Add(ctx, 1)
+}
+
 func init() {
+	ctx := context.Background()
+	initMetrics(ctx)
+
 	rootCmd.AddCommand(kbotCmd)
 
 	// Here you will define your flags and configuration settings.
